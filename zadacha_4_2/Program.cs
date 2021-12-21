a7b17561fe8a1f895b2f3e5dec049fdfcdb6536b
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-12-21 16:08:19 +0200

    Добавьте файлы проекта.

---
+++ zadacha_4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadacha_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int[,] arr = new int[int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())];
+            for (int i = 0; i < arr.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                {
+                    arr[i, j] = ((i == 0 || j == 0) ? 1 : arr[i - 1, j] + arr[i, j - 1]);
+                    Console.Write("{0,5}", arr[i, j]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
